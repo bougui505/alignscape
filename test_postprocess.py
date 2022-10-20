5f00c3965dd5eb1f9c50087d5576ce1fe183554c
IFilella <isaacfilella@gmail.com>
2022-10-20 12:35:49 +0200

    test_postprocess.py finished

--- test_postprocess.py
+++ test_postprocess.py
@@ -11,7 +11,7 @@ import newick
 import numpy as np
 import networkx as nx
 import minsptree as mspt
-
+import plot_umat
 somfile = 'som.pickle'
 timer = Timer(autoreset=True)
 
@@ -24,7 +24,7 @@ timer.start('computing localadj between queries')
 localadj, paths = mspt.get_localadjmat(som.umat,som.adj,bmus)
 timer.stop()
 timer.start('computing the tree')
-mstree,mstree_pairs,paths = mspt.get_minsptree(localadj,paths,verbose=False)
+mstree,mstree_pairs,mstree_paths = mspt.get_minsptree(localadj,paths,verbose=False)
 timer.stop()
 timer.start('computing the mstree graph')
 mspt.write_mstree_gml(mstree,som.bmus,som.labels,(som.m,som.n),outname='mstree_ntw')
@@ -32,9 +32,13 @@ timer.stop()
 timer.start('computing the unfolding')
 uumat, mapping, reversed_mapping = mspt.get_unfold_umat(som.umat, som.adj,bmus, mstree)
 timer.stop()
-exit()
 unfbmus = [mapping[bmu] for bmu in bmus]
-
-plt.matshow(uumat)
-plt.colorbar()
+timer.start('get the minsptree paths in the unfold umat')
+mstree_pairs, mstree_paths = mspt.get_unfold_msptree(mstree_pairs,mstree_paths, som.umat.shape, uumat.shape, mapping)
+timer.stop()
+timer.start('Plotting')
+labels = []
+plot_umat._plot_umat(uumat,unfbmus,labels,False)
+plot_umat._plot_msptree(mstree_pairs,mstree_paths,uumat.shape)
 plt.savefig('testunfold.pdf')
+timer.stop()
