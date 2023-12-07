ec7d66e93c627cc8d52585c771c1222d50a90351
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2023-12-07 10:57:46 +0100

    modified:   alignscape.def 	new file:   alignscape.sh

---
+++ apptainer/alignscape.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2023 Institut Pasteur                                       #
+#############################################################################
+#
+# creation_date: Thu Dec  7 10:55:40 2023
+
+set -e  # exit on error
+set -o pipefail  # exit when a process in the pipe fails
+set -o noclobber  # prevent overwritting redirection
+
+# Full path to the directory of the current script
+DIRSCRIPT="$(dirname "$(readlink -f "$0")")"
+
+singularity run $DIRSCRIPT/alignscape.sif alignscape $@
